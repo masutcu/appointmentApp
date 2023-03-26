0ebf552d5e8aa0841d17ee59a0fc32e2675ec9c0
masutcu <masutcu@gmail.com>
2023-03-26 17:28:27 +0300

    v1

---
+++ src/main/java/appointmentaplication/Appointment.java
@@ -0,0 +1,37 @@
+package appointmentaplication;
+
+import java.time.LocalDate;
+
+public class Appointment {
+    public static int counter=0;
+    private int id;
+    private String name;
+    private LocalDate date;
+
+    public Appointment(String name, LocalDate date) {
+        counter++;
+        this.id=counter;
+        this.name = name;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
